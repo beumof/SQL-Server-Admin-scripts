2acb8c781fc0879703c4ed23c42c44a734fb7a7f
beumof <40612793+beumof@users.noreply.github.com>
2019-05-31 15:57:38 +0200

    Issue Fixed

--- Backups/SQLBackups_LatestDBBackups.sql
+++ Backups/SQLBackups_LatestDBBackups.sql
@@ -4,18 +4,18 @@
 --Commentaries: Check last backup per DB in the instance
 
 --CHECK Latest DB Backups (Full, Differential and T-log)
-SELECT bu.database_name AS Database, recovery_model_Desc AS [Recovery_Model],[Full],[Differential],[Transaction_Log]
+SELECT bu.database_name, recovery_model_Desc AS [Recovery_Model],[Full],[Differential],[Transaction_Log]
 FROM (
   SELECT database_name,
   CASE [type]
     WHEN 'D' THEN 'Full'
     WHEN 'I' THEN 'Differential'
-    WHEN 'L' THEN 'Transaction Log'
+    WHEN 'L' THEN 'Transaction_Log'
   END As Type,
   max(backup_finish_date) AS lastBackup
   FROM msdb..backupset
   --WHERE description like 'TDPSQL%' -- uncomment to filter backups made with TDPSQL
   GROUP BY database_name, type
-) AS SourceTable PIVOT (max(lastBackup) FOR type IN ([Full],[Differential],[Transaction Log])) As bu
+) AS SourceTable PIVOT (max(lastBackup) FOR type IN ([Full],[Differential],[Transaction_Log])) As bu
 INNER JOIN master.sys.databases db ON bu.database_name = db.name
 ORDER BY bu.database_name
