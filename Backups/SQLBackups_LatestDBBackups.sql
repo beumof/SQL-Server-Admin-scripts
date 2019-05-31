c74cd8547ecaa1fea753052f416452a18da31b58
beumof <40612793+beumof@users.noreply.github.com>
2019-05-31 15:52:46 +0200

    Scripts redesigned to be able to filer backups per system

--- Backups/SQLBackups_LatestDBBackups.sql
+++ Backups/SQLBackups_LatestDBBackups.sql
@@ -1,6 +1,21 @@
+--File: Backups/SQLBackups_LatestDBBackups.sql
+--Compilled in https://github.com/beumof/SQL-Server-Admin-scripts
+--Added in 2019-05-31
+--Commentaries: Check last backup per DB in the instance
+
 --CHECK Latest DB Backups (Full, Differential and T-log)
-SELECT a.name AS [Database Name],a.recovery_model_Desc AS [Recovery Model],
-(SELECT MAX(b.backup_finish_date) FROM msdb..backupset b WHERE b.type = 'D' and a.name=b.database_name) AS [Full Backup],
-(SELECT MAX(b.backup_finish_date) FROM msdb..backupset b WHERE b.type = 'I' and a.name=b.database_name) AS [Diff Backup],
-(SELECT MAX(b.backup_finish_date) FROM msdb..backupset b WHERE b.type = 'L' and a.name=b.database_name) AS [Log Backup]
-FROM master.sys.databases a 
+SELECT bu.database_name AS Database, recovery_model_Desc AS [Recovery_Model],[Full],[Differential],[Transaction_Log]
+FROM (
+  SELECT database_name,
+  CASE [type]
+    WHEN 'D' THEN 'Full'
+    WHEN 'I' THEN 'Differential'
+    WHEN 'L' THEN 'Transaction Log'
+  END As Type,
+  max(backup_finish_date) AS lastBackup
+  FROM msdb..backupset
+  --WHERE description like 'TDPSQL%' -- uncomment to filter backups made with TDPSQL
+  GROUP BY database_name, type
+) AS SourceTable PIVOT (max(lastBackup) FOR type IN ([Full],[Differential],[Transaction Log])) As bu
+INNER JOIN master.sys.databases db ON bu.database_name = db.name
+ORDER BY bu.database_name
