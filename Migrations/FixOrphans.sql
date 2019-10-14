b0523e99cbcf047f5494bc7dba32d15ca897e878
beumof <40612793+beumof@users.noreply.github.com>
2019-10-14 17:41:06 +0200

    Fix orphans script

---
+++ Migrations/FixOrphans.sql
@@ -0,0 +1,23 @@
+--File: Migraations/Migrations_FixOrphans.sql
+--Extracted from: Unknown (updated with info from MS SQL official web)
+--Compilled in https://github.com/beumof/SQL-Server-Admin-scripts
+--Added in 2019-10-14
+
+SELECT 'ALTER USER ['+[name]+'] WITH LOGIN = ['+[name]+']'
+FROM sysusers
+WHERE sid IS NOT NULL 
+AND [name] NOT IN ('dbo','guest','public') 
+AND [name] NOT LIKE 'db_%' 
+AND [name] NOT LIKE 'BUILTIN%'
+
+
+/* Old version, it creates the login if it does not exist, but sp will be deprecated
+
+SELECT 'EXEC sp_change_users_login ''auto_fix'','''+[name]+''''
+FROM sysusers
+WHERE sid IS NOT NULL 
+AND [name] NOT IN ('dbo','guest','public') 
+AND [name] NOT LIKE 'db_%' 
+AND [name] NOT LIKE 'BUILTIN%'
+
+*/
