7d7e0040faa4a34bd7fb375176db24710462e389
beumof <40612793+beumof@users.noreply.github.com>
2020-04-24 18:33:05 +0200

    User scripts update

--- Users/Users_DropDBLogins.sql
+++ Users/Users_DropDBLogins.sql
@@ -1,11 +1,13 @@
---File: Users/Users_SearchForALogin.sql
---Extracted from: https://dba.stackexchange.com/questions/81595/a-query-that-lists-all-mapped-users-for-a-given-login
+--File: Users/Users_DropDBLogins.sql
+--Inspired from: https://dba.stackexchange.com/questions/81595/a-query-that-lists-all-mapped-users-for-a-given-login
 --Compilled in https://github.com/beumof/SQL-Server-Admin-scripts
---Added in 2020-04-19
+--Added in 2020-04-24
 
 
-DECLARE @loginName SYSNAME = N'loginName';
-DECLARE @DB_Users TABLE (DBName sysname, UserName sysname, LoginType sysname
+DECLARE @DBNameToDelete SYSNAME = N'psg_cam';
+
+--Generate scripts to drop DB
+DECLARE @DB_Users TABLE (DBName sysname, UserName varchar(150), LoginType sysname
 , AssociatedRole varchar(max), create_date datetime, modify_date datetime)
 
 INSERT @DB_Users
@@ -23,14 +25,20 @@ LEFT OUTER JOIN sys.database_role_members mem ON prin.principal_id=mem.member_pr
 WHERE prin.sid IS NOT NULL and prin.sid NOT IN (0x00) 
 and prin.is_fixed_role <> 1 AND prin.name NOT LIKE ''##%'''
 
-SELECT @@servername AS SQLInstance, dbname, username, logintype, create_date, modify_date,
-    STUFF((SELECT ',' + CONVERT(VARCHAR(500), associatedrole)
+;WITH RemoveLogins_CTE (SQLInstance, Username, LoginType, DBNames)
+AS (
+SELECT @@servername AS SQLInstance, username, logintype,
+    STUFF((SELECT DISTINCT ',' + CONVERT(VARCHAR(500), DBName)
         FROM @DB_Users user2
-        WHERE user1.DBName=user2.DBName AND user1.UserName=user2.UserName
+        WHERE user1.UserName=user2.UserName
         FOR XML PATH('')
-    ),1,1,'') AS Permissions_user
+    ),1,1,'') AS DBNames
 FROM @DB_Users user1
 WHERE logintype IN ('SQL_USER','WINDOWS_USER','WINDOWS_GROUP')
-AND user1.UserName = @loginName -- Commnet line for all logins in SQL instance
-GROUP BY dbname, username, logintype, create_date, modify_date
-ORDER BY DBName, username
+GROUP BY username, logintype
+)
+SELECT *, 'DROP LOGIN ['+UserName+'];' AS '--Query'
+FROM RemoveLogins_CTE
+WHERE UserName NOT LIKE 'dbo%'
+AND DBNames = @DBNameToDelete
+ORDER BY username
