5627a6fd2d57ff4dd9b3a530369e1e0ca20e4ea7
Beumof <beumof@gmail.com>
2021-01-27 11:26:20 +0100

    Snippet folder added

---
+++ Snippets/Snippet_SQLDateTimeSnippets.sql
@@ -0,0 +1,6 @@
+--File: Snippets/Snippet_SQLDateTimeSnippets.sql
+--Compilled in https://github.com/beumof/SQL-Server-Admin-scripts
+--Added in 2021-01-27
+
+-- Get Date_Time formated. IE: 20210127_102157
+SELECT CONVERT(VARCHAR(20),GETDATE(),112) + '_' + REPLACE(CONVERT(VARCHAR(20),GETDATE(),108),':','')
